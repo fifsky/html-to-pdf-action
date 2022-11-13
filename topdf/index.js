61e113cdb24ba616622cff4e5b1bdfb9e9417398
Franz Diebold <franz.diebold@tum.de>
2022-11-13 21:49:02 +0100

    Improve text rendering.

--- topdf/index.js
+++ topdf/index.js
@@ -30,6 +30,7 @@ class HTML5ToPDF {
     this.server = new Server(process.cwd())
     this.browser = await puppeteer.launch(this.options.launchOptions)
     this.page = await this.browser.newPage()
+    await this.page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
     const url = "http://localhost:3000/" + this.options.inputPath
     await this.page.goto(url, {
       waitUntil: "networkidle0",
